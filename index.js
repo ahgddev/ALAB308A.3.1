eb911d2a5be08abdeb9035adf4557f4c0d182c1b
ahgddev <ahgddev@gmail.com>
2024-02-06 20:39:34 -0700

    Cleaned up document.

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ let userDB = (database, value, uID) => {
   return new Promise((resolve, reject) => {
     resolve(database[value](uID));
     reject("This ID not in this Database.");
-});
-}
+  });
+};
 
 let userVault = (dbVault, uID) => {
   return new Promise((resolve, reject) => {
     resolve(dbVault(uID));
     reject("This ID is not in this Vault.");
-});
-}
+  });
+};
 
 let userDataProcess = (dataDB, vaultDB, ID) => {
   let user = {
@@ -24,16 +24,16 @@ let userDataProcess = (dataDB, vaultDB, ID) => {
     address: vaultDB.address,
     phone: vaultDB.phone,
     website: dataDB.website,
-    company: dataDB.company
-  }
-  return user
-}
+    company: dataDB.company,
+  };
+  return user;
+};
 
 async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
   //Part 1: The Scenario
   // const centralValue = await central(id);
@@ -41,22 +41,21 @@ async function getUserData(id) {
   // const vaultValue = await vault(id);
   // console.log(centralValue, DBValue, vaultValue)
 
-  
   //Part 2: The Implementation
   let checkID = id;
   try {
     const returnedValue = await central(id);
-      await Promise.all([
-        userDB(dbs, returnedValue, id),
-        userVault(vault, id)
-      ]).then(([db, vault]) => {
-        userDataProcess(db, vault, checkID)
-      });
+    await Promise.all([
+      userDB(dbs, returnedValue, id),
+      userVault(vault, id),
+    ]).then(([db, vault]) => {
+      userDataProcess(db, vault, checkID);
+    });
   } catch (error) {
-    console.log("User does not exist")
+    console.log("User does not exist");
   }
 }
 
 // getUserData(11)
 // getUserData(1)
-getUserData(3)
+getUserData(3);
