a03a0e67f0da7ab01677666b309ee2814677a337
ahgddev <ahgddev@gmail.com>
2024-02-06 18:48:09 -0700

    Working on pt2, gathering via the database function works.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
+let userDB = (database, value, uID) => {
+  return new Promise((resolve, reject) => {
+    resolve(database[value](uID));
+});
+}
+
 async function getUserData(id) {
   const dbs = {
     db1: db1,
@@ -10,12 +16,21 @@ async function getUserData(id) {
   // Part 1: The Scenario
   try {
     const returnedValue = await central(id);
-    let userData = await dbs[returnedValue](id);
+    userDB(dbs, returnedValue, id);
     let userVault = await vault(id);
-    console.log(userData, userVault);
+    // let promiseExecution = async () => {
+    //   let promise = await Promise.all([
+    //     firstPromise(),
+    //     secondPromise(),
+    //   ]);
+    //   console.log(promise);
+    // };
+    console.log(userDB, userVault);
   } catch (error) {
     console.log("User does not exist")
   }
 }
+
+// Async function to perform execution of all promise
 
 getUserData(1)
